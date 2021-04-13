d2ad1affe020b5b2191987fcaf11a977a8804247
GitHub Action <action@github.com>
2021-04-13 07:57:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/muwl/relxmvp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/muwl/relxmvp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.muwl.relxmvp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.muwl.relxmvp.MyBundle
 
 class MyApplicationService {
 
